61fb2ff4959ed081905b5854155781e489343903
Vinicius Amélio <vinicius.amelio@cubos.io>
2023-03-03 14:20:39 -0300

    feature: adding knobs and color options to widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,139 @@
 import 'package:blurple/themes/dark_theme.dart';
+import 'package:blurple/themes/theme_data.dart';
 import 'package:blurple/tokens/color_tokens.dart';
-import 'package:blurple/widgets/buttons/filled_button.dart';
+import 'package:blurple/widgets/buttons/buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
 
 void main() {
-  runApp(const HotreloadWidgetbook());
+  runApp(HotreloadWidgetbook());
 }
 
 class HotreloadWidgetbook extends StatelessWidget {
-  const HotreloadWidgetbook({Key? key}) : super(key: key);
+  HotreloadWidgetbook({Key? key}) : super(key: key);
+  final List<Option<Color>> colors = [
+    Option(label: "Blurple", value: ColorTokens.blurple),
+    Option(label: "Blurple Lighter", value: ColorTokens.blurpleLighter),
+    Option(label: "Blurple Darker", value: ColorTokens.blurpleDarker),
+    Option(label: "Blue", value: ColorTokens.blue),
+    Option(label: "Concrete", value: ColorTokens.concrete),
+    Option(label: "Concrete Darker", value: ColorTokens.concreteDarker),
+    Option(label: "Green", value: ColorTokens.green),
+    Option(label: "Grey", value: ColorTokens.grey),
+    Option(label: "Grey Lighter", value: ColorTokens.greyLighter),
+    Option(label: "Grey Darker", value: ColorTokens.greyDarker),
+    Option(label: "Red", value: ColorTokens.red),
+    Option(label: "Shadow", value: ColorTokens.shadow),
+    Option(label: "White", value: ColorTokens.white),
+    Option(label: "Yellow", value: ColorTokens.yellow),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Widgetbook.material(
-      scaffoldBuilder: (context, frame, child) => Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(36),
-          child: child,
+    return BlurpleThemeData.defaultTheme(
+      child: Widgetbook.material(
+        scaffoldBuilder: (context, frame, child) => Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(36),
+            child: child,
+          ),
         ),
+        categories: [
+          WidgetbookCategory(
+            name: 'buttons',
+            widgets: [
+              WidgetbookComponent(
+                name: 'Base Button',
+                useCases: [
+                  WidgetbookUseCase(
+                    name: 'Text',
+                    builder: (context) => Center(
+                      child: BaseButton.text(
+                        onPressed: () {},
+                        text: context.knobs.text(
+                          label: "Text",
+                          initialValue: "Button",
+                        ),
+                        backgroundColor: context.knobs.options(
+                            label: "Background Color", options: colors),
+                        foregroundColor: context.knobs.options(
+                            label: "Foreground Color", options: colors),
+                      ),
+                    ),
+                  ),
+                  WidgetbookUseCase(
+                    name: 'Icon',
+                    builder: (context) => Center(
+                      child: BaseButton.icon(
+                        onPressed: () {},
+                        label: context.knobs.nullableText(
+                          label: "Text",
+                          initialValue: "Button",
+                        ),
+                        foregroundColor: context.knobs.options(
+                          label: "Foreground Color",
+                          options: colors,
+                        ),
+                        backgroundColor: context.knobs.options(
+                          label: "Background Color",
+                          options: colors,
+                        ),
+                        icon: const Icon(
+                          Icons.lock_clock,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              WidgetbookComponent(
+                name: "Bordered Button",
+                useCases: [
+                  WidgetbookUseCase(
+                    name: "Text",
+                    builder: (context) => Center(
+                      child: BorderedButton(
+                        onPressed: () {},
+                        text: context.knobs.text(
+                          label: "Text",
+                          initialValue: "Button",
+                        ),
+                      ),
+                    ),
+                  ),
+                  WidgetbookUseCase(
+                    name: "Icon",
+                    builder: (context) => Center(
+                      child: BorderedIconButton(
+                        onPressed: () {},
+                        text: context.knobs.nullableText(
+                          label: "Text",
+                          initialValue: "Button",
+                        ),
+                        foregroundColor: context.knobs.options(
+                            label: "Foreground Color", options: colors),
+                        child: const Icon(Icons.wifi),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
+        themes: [
+          WidgetbookTheme(
+            name: 'Dark',
+            data: blurpleDark,
+          ),
+          WidgetbookTheme(
+            name: 'Light',
+            data: ThemeData.light(),
+          ),
+        ],
+        appInfo: AppInfo(name: 'Blurple'),
       ),
-      categories: [
-        WidgetbookCategory(
-          name: 'buttons',
-          widgets: [
-            WidgetbookComponent(
-              name: 'Base Button',
-              useCases: [
-                WidgetbookUseCase(
-                  name: 'Text',
-                  builder: (context) => BaseButton.text(
-                    onPressed: () {},
-                    text: context.knobs.text(
-                      label: "Text",
-                      initialValue: "Button",
-                    ),
-                  ),
-                ),
-                WidgetbookUseCase(
-                  name: 'Icon',
-                  builder: (context) => BaseButton.icon(
-                    onPressed: () {},
-                    label: context.knobs.nullableText(
-                      label: "Text",
-                      initialValue: "Button",
-                    ),
-                    backgroundColor: context.knobs.options(
-                      label: "Color",
-                      options: [
-                        Option(label: "Concrete", value: ColorTokens.concrete),
-                        Option(label: "Shadow", value: ColorTokens.shadow),
-                      ],
-                    ),
-                    icon: const Icon(Icons.lock_clock),
-                  ),
-                )
-              ],
-            ),
-          ],
-        )
-      ],
-      themes: [
-        WidgetbookTheme(
-          name: 'Light',
-          data: ThemeData.light(),
-        ),
-        WidgetbookTheme(
-          name: 'Dark',
-          data: blurpleDark,
-        ),
-      ],
-      appInfo: AppInfo(name: 'Example'),
     );
   }
 }
