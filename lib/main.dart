f24f068777ed1b5df8061e2faac2a768e04845b2
Vinicius Amélio <vinicius.amelio@cubos.io>
2023-03-10 17:26:33 -0300

    feature: tab bar showcase

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:blurple/themes/theme_data.dart';
 import 'package:blurple/tokens/color_tokens.dart';
 import 'package:blurple/widgets/bottom_sheets/bottom_sheet.dart';
 import 'package:blurple/widgets/buttons/buttons.dart';
+import 'package:blurple/widgets/tab/tab.dart';
+import 'package:blurple/widgets/tab/tab_item.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
 
@@ -199,6 +201,64 @@ class HotreloadWidgetbook extends StatelessWidget {
                           ),
                         );
                       }),
+                ],
+              ),
+            ],
+          ),
+          WidgetbookCategory(
+            name: "Tab Bar",
+            widgets: [
+              WidgetbookComponent(
+                name: "BlurpleTabBar",
+                useCases: [
+                  WidgetbookUseCase(
+                    name: "Default",
+                    builder: (context) {
+                      return BlurpleTabBar(
+                        items: [
+                          DefaultTabItem(
+                            isActive: false,
+                            inactiveChild: Text(
+                              "item 01",
+                              style: TextStyle(
+                                color: BlurpleThemeData.of(context)
+                                    .colorScheme
+                                    .accentColor
+                                    .withAlpha(175),
+                              ),
+                            ),
+                            child: Text(
+                              "Item 01",
+                              style: TextStyle(
+                                  color: BlurpleThemeData.of(context)
+                                      .colorScheme
+                                      .accentColor),
+                            ),
+                          ),
+                          DefaultTabItem(
+                            isActive: false,
+                            inactiveChild: Text(
+                              "item 02",
+                              style: TextStyle(
+                                color: BlurpleThemeData.of(context)
+                                    .colorScheme
+                                    .accentColor
+                                    .withAlpha(175),
+                              ),
+                            ),
+                            child: Text(
+                              "item 02",
+                              style: TextStyle(
+                                color: BlurpleThemeData.of(context)
+                                    .colorScheme
+                                    .accentColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
                 ],
               ),
             ],
